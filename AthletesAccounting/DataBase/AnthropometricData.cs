bdf92689f29d2398109f5626e783cac9c3038a86
svfits <a1d1@inbox.ru>
2016-07-20 10:39:51 +0800

    заполнил таблицу антропометрии

--- AthletesAccounting/DataBase/AnthropometricData.cs
+++ AthletesAccounting/DataBase/AnthropometricData.cs
@@ -32,10 +32,14 @@ namespace AthletesAccounting.DataBase
         /// </summary>
         public int weight { get; set; }
 
+        public string weight_evaluation { get; set; }
+
         /// <summary>
         ////рост стоя
         /// </summary>
         public int height { get; set; }
+
+        public string height_evaluation { get; set; }
 
         #region Окружность грудной клетки
 
@@ -44,20 +48,28 @@ namespace AthletesAccounting.DataBase
         /// </summary>
         public int breath { get; set; }
 
+        public string breath_evaluation { get; set; }
+
         /// <summary>
         ////выдох
         /// </summary>
         public int exhalation { get; set; }
+
+        public string exhalation_evaluation { get; set; }
 
         /// <summary>
         ///  пауза
         /// </summary>
         public int pause { get; set; }
 
+        public string pause_evaluation { get; set; }
+
         /// <summary>
         ////размах
         /// </summary>
         public int sweep { get; set; }
+
+        public string sweep_evaluation { get; set; }
 
         #endregion Окружность грудной клетки
 
@@ -68,15 +80,21 @@ namespace AthletesAccounting.DataBase
         /// </summary>
         public int rightHand { get; set; }
 
+        public string rightHand_evaluation { get; set; }
+
         /// <summary>
         /// левая кисть
         /// </summary>
         public int leftHand { get; set; }
 
+        public string leftHand_evaluation { get; set; }
+
         /// <summary>
         /// становая 
         /// </summary>
         public int stanovaya { get; set; }
+
+        public string stanovaya_evaluation { get; set; }
 
         #endregion Спирометрия Динаномо-метрия
 
@@ -87,50 +105,69 @@ namespace AthletesAccounting.DataBase
         /// </summary>
         public string skin { get; set; }
 
+        public string skin_evaluation { get; set; }
+
         /// <summary>
         /// видимые слизистые
         /// </summary>
         public string visibleMucousMembranes { get; set; }
+
+        public string visibleMucousMembranes_evaluation { get; set; }
 
         /// <summary>
         /// лимфатическая система
         /// </summary>
         public string lymphaticSystem { get; set; }
 
+        public string lymphaticSystem_evaluation { get; set; }
+
         /// <summary>
         //  жироотложение
         /// </summary>
         public string fatDeposition { get; set; }
+
+        public string fatDeposition_evaluation { get; set; }
 
         /// <summary>
         /// мускулатура
         /// </summary>
         public string  musculature { get; set; }
 
+        public string musculature_evaluation { get; set; }
+
         /// <summary>
         ////сост. груж ворот
         /// </summary>
         public string sosGrugVorot { get; set; }
+
+        public string sosGrugVorot_evaluation { get; set; }
 
         /// <summary>
         /// грудная клетка
         /// </summary>
         public string thorax { get; set; }
 
+        public string thorax_evaluation { get; set; }
+
         /// <summary>
         /// спина 
         /// </summary>
         public string back { get; set; }
+
+        public string back_evaluation { get; set; }
 
         /// <summary>
         ////стопа
         /// </summary>
         public string ream { get; set; }
 
+        public string ream_evaluation { get; set; }
+
         /// <summary>
         //// ноги
         /// </summary>
         public string foots { get; set; }
+        public string foots_evaluation { get; set; }
 
         #endregion
     }
